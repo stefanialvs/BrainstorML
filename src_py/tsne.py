43d02495022da0dea4d8fad25f96fe19bb9c0427
Kin Gtz Olivares <kin.gtz.olivares@gmail.com>
2021-03-29 19:57:02 -0400

    Fast TSNE dimensionality reduction

---
+++ src_py/tsne.py
@@ -0,0 +1,44 @@
+# import matplotlib.pyplot as plt
+import os
+import time
+import numpy as np
+
+from sklearn.datasets import load_digits
+from cuml.manifold import TSNE
+
+
+def load_glove_embeddings():
+    if not os.path.exists('./src_py/data/glove.6B.100d.npy'):
+        print('Parsing Glove Embeddings')
+        words = []
+        embeddings = []
+        with open(f'./src_py/data/glove.6B.100d.txt','r') as f:
+            for line in f:
+                values=line.split()
+                word=values[0]
+                vectors=np.asarray(values[1:],'float32')
+                words += [word]
+                embeddings += [vectors]
+        f.close()
+        words = np.vstack(words)
+        embeddings = np.vstack(embeddings)
+        np.save(f'./src_py/data/words.npy', words)
+        np.save(f'./src_py/data/glove.6B.100d.npy', embeddings)
+        return words, embeddings
+    else:
+        words = np.load('./src_py/data/words.npy')
+        embeddings = np.load('./src_py/data/glove.6B.100d.npy')
+    return words, embeddings
+
+start = time.time()
+words, embeddings = load_glove_embeddings()
+print('words.shape', words.shape)
+print('embeddings.shape', embeddings.shape)
+
+tsne = TSNE(n_components=2, perplexity=50)
+X_hat = tsne.fit_transform(embeddings)
+
+np.savetxt(f'./src_py/data/tsne_glove.csv', X_hat)
+
+print("X_hat.shape", X_hat.shape)
+print(f"time {time.time()-start}")
